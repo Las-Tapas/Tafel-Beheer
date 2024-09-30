4eb32831b909dfaf745bf9d79b5c867a8f5a0443
Smulsaus <Yourimulders2006@hotmail.com>
2024-09-30 12:19:34 +0300

    update

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=restaurant_website';
-	$user = 'root';
-	$pass = '';
+    $dsn = 'mysql:host=localhost;dbname=restaurant';
+	$user = 'restaurant';
+	$pass = 'restaurant';
 	$option = array(
 		PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
 	);
