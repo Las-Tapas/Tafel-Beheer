a8927cd3ca75985eab5cd3795e2e5b5d39767a9a
Smulsaus <Yourimulders2006@hotmail.com>
2024-10-28 10:35:04 +0100

    ss

--- restaurant.sql
+++ restaurant.sql
@@ -2,7 +2,9 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+--
 -- Table structure for table `clients`
+--
 
 CREATE TABLE `clients` (
   `client_id` int(5) NOT NULL,
@@ -13,8 +15,9 @@ CREATE TABLE `clients` (
 
 -- --------------------------------------------------------
 
+--
 -- Table structure for table `image_gallery`
-
+--
 
 CREATE TABLE `image_gallery` (
   `image_id` int(2) NOT NULL,
@@ -22,8 +25,9 @@ CREATE TABLE `image_gallery` (
   `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
 -- Dumping data for table `image_gallery`
-
+--
 
 INSERT INTO `image_gallery` (`image_id`, `image_name`, `image`) VALUES
 (1, 'Moroccan Tajine', '58146_Moroccan Chicken Tagine.jpeg'),
@@ -34,31 +38,9 @@ INSERT INTO `image_gallery` (`image_id`, `image_name`, `image`) VALUES
 
 -- --------------------------------------------------------
 
--- Table structure for table `in_order`
-
-
-CREATE TABLE `in_order` (
-  `id` int(5) NOT NULL,
-  `order_id` int(5) NOT NULL,
-  `menu_id` int(5) NOT NULL,
-  `quantity` int(3) NOT NULL DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
--- Dumping data for table `in_order`
-
-
-INSERT INTO `in_order` (`id`, `order_id`, `menu_id`, `quantity`) VALUES
-(8, 10, 16, 1),
-(9, 11, 12, 1),
-(10, 11, 16, 1),
-(11, 12, 11, 1),
-(12, 12, 12, 1),
-(13, 12, 16, 1);
-
--- --------------------------------------------------------
-
+--
 -- Table structure for table `menus`
-
+--
 
 CREATE TABLE `menus` (
   `menu_id` int(5) NOT NULL,
@@ -69,14 +51,13 @@ CREATE TABLE `menus` (
   `category_id` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
 -- Dumping data for table `menus`
-
+--
 
 INSERT INTO `menus` (`menu_id`, `menu_name`, `menu_description`, `menu_price`, `menu_image`, `category_id`) VALUES
 (1, 'Moroccan Couscous', '.', 14.00, '88737_couscous_meat.jpg', 8),
 (2, 'Beef Hamburger', '.', 3.80, 'burger.jpeg', 1),
-(3, 'Ice Cream','.', 7.50, 'summer-dessert-sweet-ice-cream.jpg', 2),
-(2, 'Beef Hamburger','.', 3.80, 'burger.jpeg', 1),
 (3, 'Ice Cream', '.', 7.50, 'summer-dessert-sweet-ice-cream.jpg', 2),
 (5, 'Coffee', '.', 10.00, 'coffee.jpeg', 3),
 (6, 'Ice Tea', '.', 3.20, '76643_ice_tea.jpg', 3),
@@ -89,16 +70,18 @@ INSERT INTO `menus` (`menu_id`, `menu_name`, `menu_description`, `menu_price`, `
 
 -- --------------------------------------------------------
 
+--
 -- Table structure for table `menu_categories`
-
+--
 
 CREATE TABLE `menu_categories` (
   `category_id` int(3) NOT NULL,
   `category_name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
 -- Dumping data for table `menu_categories`
-
+--
 
 INSERT INTO `menu_categories` (`category_id`, `category_name`) VALUES
 (1, 'burgers'),
@@ -111,23 +94,11 @@ INSERT INTO `menu_categories` (`category_id`, `category_name`) VALUES
 
 -- --------------------------------------------------------
 
--- Table structure for table `placed_orders`
-
-
-CREATE TABLE `placed_orders` (
-  `order_id` int(5) NOT NULL,
-  `order_time` datetime NOT NULL,
-  `client_id` int(5) NOT NULL,
-  `delivery_address` varchar(255) NOT NULL,
-  `delivered` tinyint(1) NOT NULL DEFAULT 0,
-  `canceled` tinyint(1) NOT NULL DEFAULT 0,
-  `cancellation_reason` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
 -- --------------------------------------------------------
 
+--
 -- Table structure for table `reservations`
-
+--
 
 CREATE TABLE `reservations` (
   `reservation_id` int(5) NOT NULL,
@@ -143,23 +114,26 @@ CREATE TABLE `reservations` (
 
 -- --------------------------------------------------------
 
+--
 -- Table structure for table `tables`
-
+--
 
 CREATE TABLE `tables` (
   `table_id` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
 -- Dumping data for table `tables`
-
+--
 
 INSERT INTO `tables` (`table_id`) VALUES
 (1);
 
 -- --------------------------------------------------------
 
+--
 -- Table structure for table `users`
-
+--
 
 CREATE TABLE `users` (
   `user_id` int(2) NOT NULL,
@@ -169,18 +143,18 @@ CREATE TABLE `users` (
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
 -- Dumping data for table `users`
-
--- admin_user -- 123456789
-
+--
 
 INSERT INTO `users` (`user_id`, `username`, `email`, `full_name`, `password`) VALUES
 (1, 'admin_user', 'user_admin@gmail.com', 'User Admin', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441');
 
 -- --------------------------------------------------------
 
+--
 -- Table structure for table `website_settings`
-
+--
 
 CREATE TABLE `website_settings` (
   `option_id` int(5) NOT NULL,
@@ -188,144 +162,132 @@ CREATE TABLE `website_settings` (
   `option_value` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
 -- Dumping data for table `website_settings`
-
+--
 
 INSERT INTO `website_settings` (`option_id`, `option_name`, `option_value`) VALUES
 (1, 'restaurant_name', 'Las Tapas'),
 (2, 'restaurant_email', 'Las.Tapas@gmail.com'),
-(3, 'admin_email', 'admin_email@gmail.com'),
+(3, 'admin_email', 'Las.Tapas@gmail.com'),
 (4, 'restaurant_phonenumber', '088866777555'),
-(5, 'restaurant_address', 'Bredeweg 235, Roermond');
+(5, 'restaurant_address', 'Bredeweg 235 Roermond');
 
+--
 -- Indexes for dumped tables
+--
 
-
+--
 -- Indexes for table `clients`
-
+--
 ALTER TABLE `clients`
   ADD PRIMARY KEY (`client_id`);
 
+--
 -- Indexes for table `image_gallery`
-
+--
 ALTER TABLE `image_gallery`
   ADD PRIMARY KEY (`image_id`);
 
--- Indexes for table `in_order`
-
-ALTER TABLE `in_order`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_menu` (`menu_id`),
-  ADD KEY `fk_order` (`order_id`);
-
+--
 -- Indexes for table `menus`
-
+--
 ALTER TABLE `menus`
   ADD PRIMARY KEY (`menu_id`),
   ADD KEY `FK_menu_category_id` (`category_id`);
 
+--
 -- Indexes for table `menu_categories`
-
+--
 ALTER TABLE `menu_categories`
   ADD PRIMARY KEY (`category_id`);
 
--- Indexes for table `placed_orders`
-
-ALTER TABLE `placed_orders`
-  ADD PRIMARY KEY (`order_id`),
-  ADD KEY `fk_client` (`client_id`);
-
+--
 -- Indexes for table `reservations`
-
+--
 ALTER TABLE `reservations`
   ADD PRIMARY KEY (`reservation_id`);
 
+--
 -- Indexes for table `tables`
-
+--
 ALTER TABLE `tables`
   ADD PRIMARY KEY (`table_id`);
 
+--
 -- Indexes for table `users`
-
+--
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`),
   ADD UNIQUE KEY `username` (`username`),
   ADD UNIQUE KEY `email` (`email`);
 
+--
 -- Indexes for table `website_settings`
-
+--
 ALTER TABLE `website_settings`
   ADD PRIMARY KEY (`option_id`);
 
+--
 -- AUTO_INCREMENT for dumped tables
+--
 
-
+--
 -- AUTO_INCREMENT for table `clients`
-
+--
 ALTER TABLE `clients`
   MODIFY `client_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
+--
 -- AUTO_INCREMENT for table `image_gallery`
-
+--
 ALTER TABLE `image_gallery`
   MODIFY `image_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
--- AUTO_INCREMENT for table `in_order`
-
-ALTER TABLE `in_order`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-
+--
 -- AUTO_INCREMENT for table `menus`
-
+--
 ALTER TABLE `menus`
   MODIFY `menu_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
+--
 -- AUTO_INCREMENT for table `menu_categories`
-
+--
 ALTER TABLE `menu_categories`
   MODIFY `category_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
--- AUTO_INCREMENT for table `placed_orders`
-
-ALTER TABLE `placed_orders`
-  MODIFY `order_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
+--
 -- AUTO_INCREMENT for table `reservations`
-
+--
 ALTER TABLE `reservations`
   MODIFY `reservation_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
+--
 -- AUTO_INCREMENT for table `tables`
-
+--
 ALTER TABLE `tables`
   MODIFY `table_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
+--
 -- AUTO_INCREMENT for table `users`
-
+--
 ALTER TABLE `users`
   MODIFY `user_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
+--
 -- AUTO_INCREMENT for table `website_settings`
-
+--
 ALTER TABLE `website_settings`
   MODIFY `option_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
+--
 -- Constraints for dumped tables
+--
 
-
--- Constraints for table `in_order`
-
-ALTER TABLE `in_order`
-  ADD CONSTRAINT `fk_menu` FOREIGN KEY (`menu_id`) REFERENCES `menus` (`menu_id`),
-  ADD CONSTRAINT `fk_order` FOREIGN KEY (`order_id`) REFERENCES `placed_orders` (`order_id`);
-
+--
 -- Constraints for table `menus`
-
+--
 ALTER TABLE `menus`
   ADD CONSTRAINT `FK_menu_category_id` FOREIGN KEY (`category_id`) REFERENCES `menu_categories` (`category_id`);
 
--- Constraints for table `placed_orders`
-
-ALTER TABLE `placed_orders`
-  ADD CONSTRAINT `fk_client` FOREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`);
 COMMIT;
